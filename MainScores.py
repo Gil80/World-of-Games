795b5d2dc11047fc32ba96a2004cd551cd1ce4f9
Gil Levy <Gil80@users.noreply.github.com>
2022-01-26 23:03:55 +0200

    updated score server

--- MainScores.py
+++ MainScores.py
@@ -11,15 +11,17 @@ error = os.path.abspath('error.html')
 app = Flask(__name__)
 @app.route('/')
 def score_server():
-    with open(scores_file,'r', encoding='utf-8') as reader:
-        current_score_string = reader.read()
-    return render_template('score.html', SCORE=current_score_string)
+    try:
+       with open(scores_file,'r', encoding='utf-8') as reader:
+           score_file_lines: list = reader.readlines()
+           first_line_raw: str = score_file_lines[0]
+           first_line: str = first_line_raw.strip()
+           score: int = int(first_line)
+           return render_template('score.html', SCORE=score)
+    except (ValueError, IndexError, FileNotFoundError):
+        return render_template('error.html', ERROR="Failed to read score")
+    
 
-@app.route('/')
-def error_server():
-    with open(scores_file,'r', encoding='utf-8') as reader:
-        current_score_string = reader.read()
-    return render_template('error.html', ERROR=error)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8777) # this line was added for running form docker https://medium.com/@rokinmaharjan/running-a-flask-application-in-docker-80191791e143
--- tests/e2e.py
+++ tests/e2e.py
@@ -1,42 +1,40 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+#from webdriver_manager.chrome import ChromeDriverManager  # Use this for local run only.
+import sys
+
+#chrome_driver = webdriver.Chrome(ChromeDriverManager().install())  # Use this for local run only.
+
+#chrome_driver = webdriver.Chrome('/home/gil/projects/WorldOfGames/chromedriver')
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument("--no-sandbox")
+chromeOptions.add_argument("--disable-dev-shm-using")
+chromeOptions.add_argument("--disable-extensions")
+#chromeOptions.add_argument(r"user-data-dir=.\cookies\\test")
+chromeOptions.headless = True
+chrome_driver = webdriver.Chrome(options=chromeOptions)  # comment this line when running locally.
+
+url = 'http://172.17.0.2:8777'
 
 
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-
+def test_score_service():
+    try:
+        chrome_driver.get(url)
+        score = int(chrome_driver.find_element(By.ID, "score").text)
+        return 0 <= score <= 1000
+    except BaseException:
+        print("cannot open the server")
 
 
 def main_function():
     if test_score_service():
-        print("Server is up")
-        return 0
+        print(0)
+        return sys.exit(0)
     else:
-        print("Server is not responding")
-        return -1
-    
-
-def test_score_service():
-    app_url = open_driver("http://172.17.0.2:8777")
-    between_range = check_score()
-    driver.quit()
-    return between_range
-
-def open_driver(url):  
-    chromeOptions = webdriver.ChromeOptions()
-    chromeOptions.add_argument("--no-sandbox")
-    chromeOptions.add_argument("--disable-dev-shm-using")
-    chromeOptions.add_argument("--disable-extensions")
-    driver.get(url)
-    app_url = driver.current_url
-    chromeOptions.headless = True
-    return app_url
+        print(-1)
+        return sys.exit(-1)
 
 
-def check_score():
-    element1 = driver.find_element_by_id("score")
-    return 1000 >= int(element1.text) >= 1
-
-
-print(main_function())
+if __name__ == '__main__':
+    main_function()
